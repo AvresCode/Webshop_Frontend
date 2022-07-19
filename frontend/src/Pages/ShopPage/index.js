79c3ec1b4e61d9f3c8ed0ffaec1f14b21d890d20
AvresCode <servas230@gmail.com>
2022-07-19 15:02:29 +0200

    Added getProducts function to shopPage

--- frontend/src/Pages/ShopPage/index.js
+++ frontend/src/Pages/ShopPage/index.js
@@ -1,4 +1,31 @@
+import {useEffect, useState} from "react"
+import axios from "axios"
+import "./style.css"
+
 const ShopPage = () => {
+
+const[productsList, setProductList] = useState([]);
+
+//sidebar elements
+const [categories, setCategories] = useState([]);
+const [rating, setRating] = useState([]);
+const [price, setPrice] = useState([])
+
+const getProducts = async () => {
+  try{
+      const response = await axios.get('http://localhost:4000/products')
+      setProductList(response.data)
+
+  } catch(e){
+    console.log(e.message)
+  }
+}
+ 
+useEffect(() => {
+  getProducts();
+}, []);
+
+
   return <div className="ShopPage">ShopPage</div>;
 };
 
